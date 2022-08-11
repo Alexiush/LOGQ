bedc744958027de10a0e53da77025fcbec01c382
Alexiush <streltsov2609@gmail.com>
2022-08-11 20:23:14 +0300

    Started RuleTemplate implementation

---
+++ LOGQ/RuleTemplate.cs
@@ -0,0 +1,13 @@
+﻿namespace LOGQ
+{
+    // Class that works almost like fact template, but must not be accepted as such an argument
+    // Possibly can have room for more patterns 
+
+    // Used in rules checking
+    // Process of rules checking can be recursive and must be more complex than linear search with facts
+
+    class RuleTemplate
+    {
+
+    }
+}
