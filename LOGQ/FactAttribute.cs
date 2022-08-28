55251405c444c79554a8cebe415050bc970683a7
Alexiush <streltsov2609@gmail.com>
2022-08-28 13:50:07 +0300

    Made marker attribute static

---
+++ LOGQ/FactAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LOGQ
+{
+    [System.AttributeUsage(System.AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public class FactAttribute : System.Attribute
+    {
+        public FactAttribute(string factName)
+        {
+            FactName = factName;
+        }
+
+        public string FactName { get; }
+    }
+}
