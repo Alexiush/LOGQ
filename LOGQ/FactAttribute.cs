dc9d6beb7475d7f5dfee642381801cbddf37782c
Alexiush <streltsov2609@gmail.com>
2022-09-04 14:17:53 +0300

    Added mapping mode parameter to fact attribute, Added fact member attribute

--- LOGQ/FactAttribute.cs
+++ LOGQ/FactAttribute.cs
@@ -5,6 +5,26 @@ using System.Text;
 namespace LOGQ
 {
     /// <summary>
+    /// Enum with mapping modes that define which class members will be used when mapping to fact
+    /// </summary>
+    public enum MappingMode
+    {
+        PublicProperties,
+        AllProperties,
+        PublicFields,
+        AllFields,
+        PublicPropertiesAndFields,
+        AllPropertiesAndFields,
+        MarkedData
+    }
+
+    /// <summary>
+    /// Marker attribute for class members that will be used in mapping to fact
+    /// </summary>
+    [System.AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    public class FactMemberAttribute : System.Attribute {}
+
+    /// <summary>
     /// Marker attribute for mapper that creates fact and rule representations of a class
     /// </summary>
     [System.AttributeUsage(System.AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
@@ -14,11 +34,13 @@ namespace LOGQ
         /// Creates marker attribute for mapper
         /// </summary>
         /// <param name="factName">Name that will be used by mapper (mapper will add prefixes to given name)</param>
-        public FactAttribute(string factName)
+        public FactAttribute(string factName, MappingMode mappingMode = MappingMode.PublicProperties)
         {
             FactName = factName;
+            MappingMode = mappingMode;
         }
 
         public string FactName { get; }
+        public MappingMode MappingMode { get; }
     }
 }
