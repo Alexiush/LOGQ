8173b40dbd3400df4990e425177a1c4947ea9938
Alexiush <streltsov2609@gmail.com>
2022-09-21 13:40:33 +0300

    Switched from Generation namespace to origin class namespaces

--- LOGQ Source Generation/LOGQ Source Generation/FactsGenerator.cs
+++ LOGQ Source Generation/LOGQ Source Generation/FactsGenerator.cs
@@ -35,13 +35,15 @@ namespace LOGQ_Source_Generation
     {
         public readonly string OriginName;
         public readonly string Name;
+        public string Namespace;
         public readonly List<Property> Properties;
         public bool CanBeIndexed;
 
-        public GenerationData(string originName, string name, List<Property> properties, bool canBeIndexed)
+        public GenerationData(string originName, string name, string nameSpace, List<Property> properties, bool canBeIndexed)
         {
             OriginName = originName;
             Name = name;
+            Namespace = nameSpace;
             Properties = properties;
             CanBeIndexed = canBeIndexed;
         }
@@ -82,6 +84,51 @@ namespace LOGQ_Source_Generation
             {
                 return FieldTypeReciever(member);
             }
+        }
+
+        // determine the namespace the class/enum/struct is declared in, if any
+        static string GetNamespace(BaseTypeDeclarationSyntax syntax)
+        {
+            // If we don't have a namespace at all we'll return an empty string
+            // This accounts for the "default namespace" case
+            string nameSpace = string.Empty;
+
+            // Get the containing syntax node for the type declaration
+            // (could be a nested type, for example)
+            SyntaxNode? potentialNamespaceParent = syntax.Parent;
+
+            // Keep moving "out" of nested classes etc until we get to a namespace
+            // or until we run out of parents
+            while (potentialNamespaceParent != null &&
+                    potentialNamespaceParent is not NamespaceDeclarationSyntax
+                    && potentialNamespaceParent is not FileScopedNamespaceDeclarationSyntax)
+            {
+                potentialNamespaceParent = potentialNamespaceParent.Parent;
+            }
+
+            // Build up the final namespace by looping until we no longer have a namespace declaration
+            if (potentialNamespaceParent is BaseNamespaceDeclarationSyntax namespaceParent)
+            {
+                // We have a namespace. Use that as the type
+                nameSpace = namespaceParent.Name.ToString();
+
+                // Keep moving "out" of the namespace declarations until we 
+                // run out of nested namespace declarations
+                while (true)
+                {
+                    if (namespaceParent.Parent is not NamespaceDeclarationSyntax parent)
+                    {
+                        break;
+                    }
+
+                    // Add the outer namespace as a prefix to the final namespace
+                    nameSpace = $"{namespaceParent.Name}.{nameSpace}";
+                    namespaceParent = parent;
+                }
+            }
+
+            // return the final namespace
+            return nameSpace;
         }
 
         /// <summary>
@@ -249,8 +296,9 @@ namespace LOGQ_Source_Generation
                     }
                 }
 
+                string classNamespace = GetNamespace(classDeclarationSyntax);
                 // Create a GenerationData for use in the generation phase
-                classesToGenerate.Add(new GenerationData(classSymbol.ToDisplayString(), className, properties, canBeIndexed));
+                classesToGenerate.Add(new GenerationData(classSymbol.ToDisplayString(), className, classNamespace, properties, canBeIndexed));
             }
 
             return classesToGenerate;
